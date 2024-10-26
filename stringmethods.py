a1a02b5d3685133af7bf8903a8cd04dcb278d76d
suyogzz78 <139199339+suyogzz78@users.noreply.github.com>
2024-10-26 15:38:49 +0545

    Add files via upload

---
+++ stringmethods.py
@@ -0,0 +1,14 @@
+# strings are immutable
+l1="##valorant###"
+print(len(l1))
+print(l1.upper())
+print(l1.lower())
+print(l1.rstrip("#"))
+print(l1.replace("valorant","bgmi"))
+str=("hello everyone")
+print(len(str))
+print(len(str.center(50)))
+print(l1.count("valorant"))
+str1=("we are pleased to welcome you!")
+print(str1.endswith("!"))
+print(str1.endswith("are",2,6))
---
+++ strings.py
@@ -0,0 +1,10 @@
+name="suyog"
+sg= '''hi,
+how are you'''
+print("hello,"+name)
+print(name)
+print(name[4])
+print("lets use for loop")
+for character in sg:
+    print(character)
+    
