a763cd687938debff813261279df71315d807633
suyogzz78 <sakurachhetri97@gmail.com>
2024-10-30 15:42:53 +0545

    whileloop

---
+++ whileloop.py
@@ -0,0 +1,13 @@
+'''players = ['ronaldo', 'messi', 'neymar', 'suarez']
+i = 0
+while i < len(players):
+    print(players[i])
+    i += 1'''
+i=5
+while(i > 0):
+ print(i)
+ i=i-1
+else:
+ print("shit")
+
+
