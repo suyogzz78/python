be1eecaf3b5b06508f5badde4e57bbb70028dd9e
suyogzz78 <sakurachhetri97@gmail.com>
2024-11-05 14:23:08 +0545

    funcarg

---
+++ funcarg.py
@@ -0,0 +1,11 @@
+# def name(fname,mname,lname):
+#     print("hey!",fname,mname,lname)
+
+# name("suyog","bahadur","gautam")
+# name("imon","chuike","panta")
+
+
+def sum(a,b,c=1):
+    print("the sum is ",a+b)
+
+sum(1,9)
